f30bf86da0a30ff8847d419334f8cef3a02fb46c
Dylan <d.barva5402@gmail.com>
2020-08-08 21:05:07 -0700

    why was i using vectors

--- src/p1.cpp
+++ src/p1.cpp
@@ -3,24 +3,20 @@
 //
 
 #include "p1.h"
-#include <math.h>
 #include <iostream>
-#include <vector>
-#include <numeric>
 
 namespace p1 {
 
     void solve() {
-        std::vector<int> sum;
+        int sum = 0;
         for (int i = 3; i < 1000; i += 3) {
-            sum.push_back(i);
+            sum += i;
         }
         for (int a = 5; a < 1000; a += 5) {
             if (a % 3 !=  0) {
-                sum.push_back(a);
+                sum += a;
             }
         }
-        std::cout << std::accumulate(sum.begin(), sum.end(), 0);
-
+        std::cout << sum;
     }
 }
