fb261d1ddce9d3ca24ec7a342f248e14a78f9020
Dylan <d.barva5402@gmail.com>
2020-08-14 22:46:09 -0700

    finish problem 8

--- include/p8.h
+++ include/p8.h
@@ -5,9 +5,13 @@
 #ifndef EULER_CPP_P8_H
 #define EULER_CPP_P8_H
 
+#include <string>
+
 namespace p8 {
 
     void solve();
+
+    long multiply_digits(std::string num);
 
 }
 #endif //EULER_CPP_P8_H
--- src/p8.cpp
+++ src/p8.cpp
@@ -13,22 +13,29 @@ namespace p8 {
     void solve() {
         std::string long_num = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
         std::vector<int> num_vector;
-//        std::cout << std::stoi(long_num.substr(0, 1));
-//        for (int i = 0; i < long_num.size(); i++) {
-//            num_vector.push_back(std::stoi(long_num.substr(i, 1)));
-//        }
-//        print_vector(num_vector);
-//        int num_adjacent_digits = 13;
-        int num_adjacent_digits = 4;
-        std::string num;
-        for (int a = num_adjacent_digits; a < 10; a++) {
-//        for (int a = num_adjacent_digits - 1; a < long_num.size(); a++) {
-            for (int b = a; b > )
-
+        int num_adjacent_digits = 13;
+        std::string num = "";
+        long biggest_product = 0;
+        long product = 0;
+        for (int a = num_adjacent_digits; a < 1000; a++) {
+            for (int b = num_adjacent_digits; b > 0; b--) {
+                num += long_num.substr(a - b, 1);
             }
-//            num = long_num.substr(a - 4, 1) + long_num.substr(a - 3, 1) + long_num.substr(a - 2, 1) +long_num.substr(a - 1, 1);
-            std::cout << num << "\n";
+            product = multiply_digits(num);
+            if (product > biggest_product) {
+                biggest_product = product;
+            }
+            num = "";
         }
+        std::cout << biggest_product;
 
+    }
+
+    long multiply_digits(std::string num) {
+        long product = 1;
+        for (int i = 0; i < num.size(); i++) {
+            product *= std::stoi(num.substr(i, 1));
+        }
+        return product;
     }
 }
